d3bd31d0bfe2732b2905c2942a8e4ad68fb73c07
Gabriel Touron <gtcode@pm.me>
2020-05-11 22:46:55 +0200

    Clean code

--- src/Arena.java
+++ src/Arena.java
@@ -2,22 +2,25 @@ public class Arena {
 
     public static void main(String[] args) {
         //Set monster name and life points here
-        Monster blueCorner = new Monster("Dragon",100);
-        Monster rightCorner = new Monster("Minotor",90);
+        Monster blueCorner = new Monster("Dragon",150);
+        Monster redCorner = new Monster("Minotor",80);
 
         while (true) {
             //Set monsters damage here
-            blueCorner.attack(10, rightCorner);
-            rightCorner.attack(12, blueCorner);
-            if ((blueCorner.life == 0 || blueCorner.life < 0) && (rightCorner.life == 0 || rightCorner.life < 0)) {
+            blueCorner.attack(15, redCorner);
+            redCorner.attack(11, blueCorner);
+            //Equality
+            if ((blueCorner.life == 0 || blueCorner.life < 0) && (redCorner.life == 0 || redCorner.life < 0)) {
                 System.out.printf("Monsters are KO ! this arena is full of blood");
                 break;
             }
+            //RedCorner is the winner
             if (blueCorner.life == 0 || blueCorner.life < 0 ) {
-                System.out.printf("%s is the winner !", rightCorner.name);
+                System.out.printf("%s is the winner !", redCorner.name);
                 break;
             }
-            if (rightCorner.life == 0 || rightCorner.life < 0) {
+            //RueCorner is the winner
+            if (redCorner.life == 0 || redCorner.life < 0) {
                 System.out.printf("%s is the winner !", blueCorner.name);
                 break;
             }
